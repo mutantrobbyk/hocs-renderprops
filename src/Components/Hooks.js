a380a5aba5213e1b0561493d645ac88ec14dfb14
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-12-13 14:36:12 -0700

    more changes, added an axios call and changed pokemon data

--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -9,7 +9,7 @@
 //     }
 
 //     handleAdd(){
-    // this.setState({ count: count + 1})
+// this.setState({ count: count + 1})
 //}
 
 //     render(){
@@ -23,32 +23,37 @@
 
 // export default Hooks;
 
-import React, {useState, useEffect} from 'react'
-import NameDisplay from './NameDisplay'
+import React, { useState, useEffect } from "react";
+import NameDisplay from "./NameDisplay";
+import axios from "axios";
 const Hooks = () => {
-    const [count, setCount] = useState(0)
-    if (count < 0){
-        setCount(10000)
-    }
-    if (count > 10000) {
-        setCount(0)
-    }
-    const [name, setName] = useState('')
-    useEffect(() => {
-        console.log('useEffect Invoked')
-    }, [])
-    useEffect(() => {
-        console.log('Count useEffect Invoked')
-    }, [count])
-    return(
-        <div>
-            <NameDisplay name = {name}/>
-            <input value={name} onChange={(e) => setName(e.target.value)} type="text"/>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
+  if (count < 0) {
+    setCount(10000);
+  }
+  if (count > 10000) {
+    setCount(0);
+  }
+  const [name, setName] = useState("");
+  const [pokemon, setPokemon] = useState({});
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/1").then(res => {
+      setPokemon(res.data);
+    });
+  }, []);
+  useEffect(() => {
+    console.log("Count useEffect Invoked");
+  }, [count]);
+  return (
+    <div>
+      <h1>{pokemon.name}</h1>
+      <NameDisplay name={name} />
+      <input value={name} onChange={e => setName(e.target.value)} type="text" />
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
 
-export default Hooks
+export default Hooks;
